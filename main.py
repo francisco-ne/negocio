513ec1f04ce1f839b628451f23dd28425b3e34d1
francisco-ne <negrefrancisco@gmail.com>
2025-10-24 20:49:04 -0300

    cree un metodo con descuentos

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 class Producto:
     def __init__(self, nombre, precio, stock):
+        if precio < 0 or stock < 0:
+            raise ValueError("el precio y el stock deben ser mayores o iguales a 0")
         self.nombre = nombre
         self.precio = precio
         self.stock = stock
+
+    def agregar_descuento(self, porcentaje):
+        if porcentaje < 0 or porcentaje > 100:
+            raise ValueError("el porcentaje debe estar entre un 0% y 100%")
+        descuento = self._precio * (porcentaje/100)
+        self._precio -= descuento
+
+    
